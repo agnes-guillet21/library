fdfcd0d640855271b8090c0d10570aff90149f14
agnes-guillet21 <agnes.guillet@hotmail.fr>
2025-01-27 22:07:54 +0100

    Front: add httpClientModule

--- library-front/src/app/app.component.ts
+++ library-front/src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Book } from './models/book';
 import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
-    RouterOutlet],
+    RouterOutlet,
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
