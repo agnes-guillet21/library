d5c20a917986670cea0c8f9d84dbba83ffc25948
agnes-guillet21 <agnes.guillet@hotmail.fr>
2025-01-27 22:07:16 +0100

    Front: Create route

--- library-front/src/app/landing-page/landing-page.component.ts
+++ library-front/src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
 
+  constructor(private router: Router ){}
+
+  onContinue(){
+    this.router.navigateByUrl('books');
+
+  }
 }
