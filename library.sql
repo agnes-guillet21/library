9cc6fe8a13c062014e0982e3eb5944db090b6b5c
agnes-guillet21 <agnes.guillet@hotmail.fr>
2025-01-27 22:09:04 +0100

    Back : fix error

--- library.sql
+++ library.sql
@@ -7,10 +7,12 @@ CREATE TABLE book (
   isbn INT NOT NULL
 );
  
-INSERT INTO book (title, description, description, author,isbn) VALUES
-  ('rebelle', 'description en cours', 'untel', '1'),
-  ('livre2', 'description2', 'auteur2', '2'),
-  ('livre3', 'description3', 'auteur3', '3');
-  ('livre4', 'description4', 'auteur4', '3');
-  ('livre5', 'description5', 'auteur5', '3');
-  ('livre6', 'description6', 'auteur6', '3');
+INSERT INTO book (title, description, author,isbn) VALUES
+  ('rebelle','description en cours','untel','1'),
+  ('livre2','description2','auteur2','2'),
+  ('livre3','description3','auteur3','3'),
+  ('livre4','description4','auteur4','4'),
+  ('livre5','description5','auteur5','5'),
+  ('livre6','description6','auteur6','6');
+
+
