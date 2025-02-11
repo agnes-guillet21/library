16ace52a6e6efcc8273aa885b041142a4f261967
agnes-guillet21 <agnes.guillet@hotmail.fr>
2025-02-11 11:33:42 +0100

    Front : BookService : create promise for delete methode

--- library-front/src/app/services/book.service.ts
+++ library-front/src/app/services/book.service.ts
@@ -1,7 +1,9 @@
-import { inject, Injectable } from "@angular/core";
+import { BookConstants } from './../constants/bookConstants';
+import { Injectable } from "@angular/core";
 import { Book } from "../models/book";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
+
 
 
 @Injectable({
@@ -12,12 +14,38 @@ export class BookService {
 
   constructor(private http : HttpClient){}
 
-  getBooks(): Observable<Book[]>{
-    return this.http.get<Book[]>('http://localhost:9000/books');
+  public getBooks(): Observable<Book[]>{
+    return this.http.get<Book[]>(BookConstants.getBooksApiEndpoint);
   }
 
-  getBookById(bookId : number): Observable<Book> {
-    return this.http.get<Book>(`http://localhost:9000/books/${bookId}`);
+  public getBookById(bookId : number): Observable<Book> {
+    return this.http.get<Book>(`${BookConstants.getBooksApiEndpoint}/${bookId}`);
   }
+
+  public createBook(datas: Book){
+      this.http.post(BookConstants.getBooksApiEndpoint, datas)
+      .subscribe();// ds le susb traiter l erreur
+  }
+
+   public updateBook(datas: Book){
+    this.http.put(BookConstants.getBooksApiEndpoint, datas)
+    .subscribe();
+   }
+// TODO  si la suppression ne se fait pas afficher un message utilisateur
+   public deleteBook(bookId : number) : Promise<void> {
+    const promise = new Promise<void>((resolve) =>{
+      this.http.delete<void>(`${BookConstants.getBooksApiEndpoint}/${bookId}`, {
+        headers : {
+      'Content-Type':'application/json,charset=utf-8'
+        }
+      })
+      .subscribe({
+        complete() {
+          resolve();
+        }
+      });
+    });
+    return promise;
+   }
 
 }
