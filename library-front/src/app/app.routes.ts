c8eb2f9a6857a4cc25c1d357b354f5348384bce5
agnes-guillet21 <agnes.guillet@hotmail.fr>
2025-02-11 11:36:27 +0100

    Front : add path

--- library-front/src/app/app.routes.ts
+++ library-front/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
-import { BookListComponent } from './book-list/book-list.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SingleBookComponent } from './single-book/single-book.component';
+import { BookListComponent } from './book-components/book-list/book-list.component';
+import { LandingPageComponent } from './home-component/landing-page/landing-page.component';
+import { SingleBookComponent } from './book-components/single-book/single-book.component';
+import { BookFormComponent } from './book-components/book-form/book-form.component';
 
 export const routes: Routes = [
   {path:'books/:id',component: SingleBookComponent },
   {path: 'books', component: BookListComponent},
-  {path: '', component: LandingPageComponent}
+  {path: '', component: LandingPageComponent},
+  {path:'create', component: BookFormComponent},
+  {path:'update/:id', component: BookFormComponent}
 ];
