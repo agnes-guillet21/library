91164f28d5ca6f51c4e00e3dd70524726a5ceafc
agnes-guillet21 <agnes.guillet@hotmail.fr>
2025-02-11 11:37:03 +0100

    Front : trying to import Interceptor : WIP

--- library-front/src/app/app.config.ts
+++ library-front/src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {  provideHttpClient, withFetch,  } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { ErrorInterceptor } from './services/errorInterceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient(withFetch()),
-    { provide: LOCALE_ID, useValue: 'fr-FR' }
+    ErrorInterceptor,
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
   ]
 };
