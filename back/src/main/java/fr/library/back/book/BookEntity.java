6fe67ba6e3183320e182e039712017407c040015
agnes-guillet21 <agnes.guillet@hotmail.fr>
2025-03-24 11:27:46 +0100

    Back : book : use @data annotation from lombok

--- back/src/main/java/fr/library/back/book/BookEntity.java
+++ back/src/main/java/fr/library/back/book/BookEntity.java
@@ -4,19 +4,13 @@ package fr.library.back.book;
 import fr.library.back.author.AuthorEntity;
 import fr.library.back.image.ImageEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 import java.util.Set;
 
 @Entity(name = "book")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class BookEntity {
 
     @Id
